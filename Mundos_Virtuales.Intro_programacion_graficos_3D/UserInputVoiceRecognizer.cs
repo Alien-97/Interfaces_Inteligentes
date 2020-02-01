04e2de2a9874773f641863170188ad465a052e9c
Alien-97 <alu0101035406@ull.edu.es>
2020-02-01 12:30:10 +0000

    resumen vuforia

---
+++ Mundos_Virtuales.Intro_programacion_graficos_3D/UserInputVoiceRecognizer.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.UI;
+
+public class UserInputVoiceRecognizer : MonoBehaviour
+{
+	
+	
+	
+	bool teclaPulsada = false;
+
+	float windowTime = 1; 
+
+	[SerializeField]
+	    private Text m_Hypotheses;
+
+	[SerializeField]
+	    private Text m_Recognitions;
+
+	    private DictationRecognizer m_DictationRecognizer;
+
+	[SerializeField]
+	    private string[] m_Keywords;
+
+	    private KeywordRecognizer m_Recognizer; //el que le pasas la lista de palabras a reconocer
+	    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void  OnPhraseRecognized(PhraseRecognizedEventArgs args){
+
+    	StringBuilder builder = new StringBuilder();
+        builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
+        builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
+        builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
+        Debug.Log(builder.ToString());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+		
+		if (Time.time - windowTime >= 1){
+	        if(Input.GetKey(KeyCode.UpArrow)){
+
+	        	if(teclaPulsada == false){
+
+	        		teclaPulsada = true;
+		        	m_Keywords = new string[4];
+		       		m_Keywords.SetValue("Hello",0);
+		        	m_Keywords.SetValue("Good",1);
+		        	m_Keywords.SetValue("Football",2);
+		        	m_Keywords.SetValue("Interfaces",3);
+
+		        	m_Recognizer = new KeywordRecognizer(m_Keywords);
+
+		        
+		        	m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+		        	Debug.Log("Estoy empezando a escuchar ");
+		        	m_Recognizer.Start();
+		        	Debug.Log("Estoy terminando de escuchar ");
+		        }
+		        else if(teclaPulsada == true){
+
+		        	m_DictationRecognizer.Stop();
+		        	m_Recognizer.Dispose();
+
+		        	Debug.Log("Tecla Pulsada,en m_Recognizer, true");
+		        	teclaPulsada = false;
+
+		        }
+
+		        windowTime = Time.time;
+		    } else if(Input.GetKey(KeyCode.DownArrow)) {
+		    	if(teclaPulsada == false){
+		    		teclaPulsada = true;
+		        	m_DictationRecognizer = new DictationRecognizer();
+
+			        m_DictationRecognizer.DictationResult += (text, confidence) =>
+			        {
+			        	try{
+				            Debug.LogFormat("Dictation result: {0}", text);
+				            m_Recognitions.text += text + "\n";
+			        	}
+			        	catch(Exception e){}
+			        };
+
+			        m_DictationRecognizer.DictationHypothesis += (text) =>
+			        {
+			        	try{
+				            Debug.LogFormat("Dictation hypothesis: {0}", text);
+				            m_Hypotheses.text += text;
+			        	}
+			        	catch(Exception e){}
+			        };
+
+			        m_DictationRecognizer.DictationComplete += (completionCause) =>
+			        {
+			            if (completionCause != DictationCompletionCause.Complete)
+			                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+			        };
+
+			        m_DictationRecognizer.DictationError += (error, hresult) =>
+			        {
+			            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+			        };
+
+			        m_DictationRecognizer.Start();
+
+			        
+			    }
+			    else if(teclaPulsada == true){
+
+			    	m_DictationRecognizer.Stop();
+			    	m_DictationRecognizer.Dispose();
+
+			    	Debug.Log("Tecla pulsada,m_DictationRecognizer,true");
+			    	teclaPulsada = false;
+			    }
+			    windowTime = Time.time;
+	        }
+
+    	}
+    }
+}
--- Vuforia/README.md
+++ Vuforia/README.md
@@ -3,3 +3,7 @@
 * Autores: Alien Embarec Riadi, Antonio Chávez López
 
 * Fecha: 15/11/2019
+
+## Resumen
+
+El software Vuforia nos permite crear proyectos de Realidad Aumentada, utilizando imágenes para saber dónde se tienen que crear los elementos. En nuestro caso, la imagen era de un peón de ajedrez, y el programa se encargaba de crear objetos alrededor de ella, como objeto elegimos un  GameObject de Unity que era una esfera, que se situaba alrededor del peón.
